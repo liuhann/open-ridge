928182b5a3fed948f007a07aeef603ac35ba7dcb
Liu Han <liuhann@gmail.com>
2026-03-19 18:41:21 +0800

    增加debug图标

--- core/editor/src/panels/left-nav/LeftNav.jsx
+++ core/editor/src/panels/left-nav/LeftNav.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import NavBar from '../../components/NavBar/NavBar.jsx'
 
-
 const LeftNav = () => {
   return (
     <NavBar
@@ -9,6 +8,8 @@ const LeftNav = () => {
         icon: <i className='bi bi-folder2' />
       }, {
         icon: <i className='bi bi-grid-1x2' />
+      }, {
+        icon: <i class='bi bi-caret-right-square' />
       }]}
       bottoms={[{
         icon: <i className='bi bi-gear' />
