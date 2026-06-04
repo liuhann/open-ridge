79d6a3e9bc8531a83b2ab8cc87427ab95936dde1
Liu Han <liuhann@gmail.com>
2026-06-04 11:13:17 +0800

    修改构建

--- packages/editor/webpack.prod.js
+++ packages/editor/webpack.prod.js
@@ -6,7 +6,19 @@ module.exports = merge(config, {
   devtool: false,
   optimization: {
     minimize: true,
-    usedExports: true
+    usedExports: true,
+    // concatenateModules: false, // 关闭作用域提升，取消concat合并
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+      // 把通用组件/公共代码拆分独立chunk
+        common: {
+          name: 'common',
+          minChunks: 2, // 被>=2处引入就抽离
+          chunks: 'all'
+        }
+      }
+    }
     // splitChunks: {
     //   chunks: 'all',
     //   name: 'vendor'
